88ae1537a7047d55b6e8b2f55ebcb261e4a44ae7
shivank0927 <shivang.arya09@gmail.com>
2025-04-08 15:41:32 +0530

    prisoner's dilemma

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import sys
 import os
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # don't know
 from simulation.simulator import *
 
+while True:
+    try:
+        counts = int(input("Enter simulation counts"))
+        break
 
-display = simulations(1)
+    except Exception as e:
+        print(e, "\n")
 
+display = simulations(counts)
 print(display)
